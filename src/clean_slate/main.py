6078ba668255a680bbbb789bb680cdeb5fa06e3e
Angelika Brown <angelikpbrown@gmail.com>
2026-07-21 18:35:54 -0400

    cleaningduplicates

--- src/clean_slate/main.py
+++ src/clean_slate/main.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 
-
+#read the CSV file
 try:
-
+    #saved in dataframe
     df = pd.read_csv('sample_data/testdata.csv')
 
     print(f"Imported {len(df)} records")
@@ -16,4 +16,20 @@ try:
 
 except FileNotFoundError:
     print("File not found. Please check the file path.")
+
+
+#REMOVE DUPLICATES
+
+#count original records
+original_count = len(df)
+
+df.drop_duplicates(inplace=True)
+
+new_count = len(df)
+
+#summary
+print("\n Clean Slate")
+print(f"Imported:        {original_count}")
+print(f"Duplicates:      {original_count - new_count}")
+print(f"Cleaned Records: {new_count}")
 
